11cb9ad04bc688fe14b554e97f9f62b6f4fe2723
Fabian Fregin <fabian.fregin@gmail.com>
2023-11-14 13:28:12 +0100

    add project

---
+++ src/main/java/wortsuche/Wortsuche/RatselApplication.java
@@ -0,0 +1,13 @@
+package wortsuche.Wortsuche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatselApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatselApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wortsuche/Wortsuche/RatselApplicationTests.java
@@ -0,0 +1,13 @@
+package wortsuche.Wortsuche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatselApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
