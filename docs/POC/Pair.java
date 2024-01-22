14d8849658b49138b810df9a57a9d6fd4d290e43
Fabian Fregin <fabian.fregin@gmail.com>
2024-01-22 10:59:45 +0100

    strukturierung

---
+++ docs/POC/Pair.java
@@ -0,0 +1,30 @@
+
+public class Pair {
+    String word;
+    String place;
+    String direction;
+    int[] indices;
+
+    public Pair(String word, String place, String direction, int[] indices) {
+        this.word = word;
+        this.place = place;
+        this.direction = direction;
+        this.indices = indices;
+    }
+    
+    public String getWord() {
+        return word;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+  
+    public int[] getIndices() {
+        return indices;
+    }
+}
